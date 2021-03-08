fde36a132057bb50d2642a4cbb591941912a1821
Lauren Eng <73508837+littledumpleng@users.noreply.github.com>
2021-03-07 20:38:55 -0500

    Cedited lauren-script.js and connected api

--- public/lauren-script.js
+++ public/lauren-script.js
@@ -1,4 +1,16 @@
+
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const restaurants = [];
+
 const search = document.querySelector("#search"); // document is html page
 search.addEventListener('keyup', async (event) => { // keyup is stop typing
     alert("hello");
+    // sesarch endpoint
 });
+
+
+// async function windowActions() { // asynchronous function; async gives access to await keyword
+//     const request = await fetch('/api');
+//     const data = await request.json(); // data is request formatted to json
+//     console.table(data);
+// }
